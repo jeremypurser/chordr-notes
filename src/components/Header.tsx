a97c426d93e53367f613b2409008dd8b8d0c32f0
Jeremy Purser <jeremypurser@gmail.com>
2020-02-22 13:10:51 -0500

    Create Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <section className="hero">
+      <div className="hero-body">
+        <h1 className="title">Chordr Notes</h1>
+      </div>
+    </section>
+  );
+}

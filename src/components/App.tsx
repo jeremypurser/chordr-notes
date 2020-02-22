9c41a970796c641c845dcd92e20b167aafcb2e0e
Jeremy Purser <jeremypurser@gmail.com>
2020-02-22 12:17:40 -0500

    Import bulma
    Add style classes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import GuitarNeck from './GuitarNeck';
+import 'bulma';
 
 export default function App() {
   return (
-    <div>
-      <GuitarNeck />
-      <GuitarNeck />
+    <div className="level">
+      <div className="level-left">
+        <GuitarNeck />
+      </div>
     </div>
   );
 }

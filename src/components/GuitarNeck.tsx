9d24705184ea9e301145982d32fd3506cb458f20
Jeremy Purser <jeremypurser@gmail.com>
2020-02-22 13:13:32 -0500

    Use helper function stringCases

--- src/components/GuitarNeck.tsx
+++ src/components/GuitarNeck.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { State } from '../model';
+import { stringCases } from '../util';
+import '../styles/GuitarNeck.scss';
 
-export default function GuitarNeck() {
+export default function GuitarNeckComponent() {
+  // pulls in state from Redux store
   const guitarNeck = useSelector((state: State) => state.guitarNeck);
-  return (
-    <div>
-      {guitarNeck.map(fret =>
-        fret.map((string, j) => <span key={j}>{string}</span>)
-      )}
+
+  const renderGuitarNeck = guitarNeck.map((fret, i) => (
+    <div key={i}>
+      {fret.map((string, j) => (
+        <div className={`column ${stringCases(string)}`} key={j}>
+          {string}
+        </div>
+      ))}
     </div>
-  );
+  ));
+
+  return <section className="columns">{renderGuitarNeck}</section>;
 }

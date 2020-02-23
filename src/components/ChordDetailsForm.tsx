9a441de7bce2da73d05c7d1b4e8454591d6bc569
Jeremy Purser <jeremypurser@gmail.com>
2020-02-23 17:55:51 -0500

    Scaffold props to be resuable

--- src/components/ChordDetailsForm.tsx
+++ src/components/ChordDetailsForm.tsx
@@ -1,5 +1,19 @@
-import React from 'react';
+import React, { MouseEvent } from 'react';
+import { default as SaveButton } from './Button';
+import '../styles/ChordDetailsForm.scss';
 
 export default function ChordDetailsForm() {
-  return <div id="details">hellloooo</div>;
+  const handleSubmit = (e: MouseEvent) => {
+    console.log(e);
+  };
+
+  return (
+    <div className="field">
+      <label className="label">Chord Name</label>
+      <div className="control details">
+        <input className="input" type="text" placeholder="G major" />
+      </div>
+      <SaveButton clickHandler={handleSubmit} text="Save" />
+    </div>
+  );
 }

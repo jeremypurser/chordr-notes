91ecc1983655f650f5b62e9dc454564b35cafc8d
Jeremy Purser <jeremypurser@gmail.com>
2020-02-23 18:10:10 -0500

    Factor out interface

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,10 @@
 import React, { MouseEvent } from 'react';
 
-export default function Button({
-  text,
-  clickHandler,
-}: {
+interface ButtonProps {
   text: string;
   clickHandler: (e: MouseEvent) => void;
-}) {
+}
+export default function Button({ text, clickHandler }: ButtonProps) {
   return (
     <div className="control">
       <button onClick={clickHandler} className="button is-primary">

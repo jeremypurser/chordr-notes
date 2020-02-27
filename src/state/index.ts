3fb62f94e3d4aefb39d4e1214dd56aff25ab997e
Jeremy Purser <jeremypurser@gmail.com>
2020-02-26 20:12:13 -0500

    Export chord name slice

--- src/state/index.ts
+++ src/state/index.ts
@@ -2,5 +2,6 @@ export {
   guitarNeckSlice,
   initialState as guitarNeckState,
 } from './guitar-neck-slice';
+export { chordNameSlice } from './chord-name-slice';
 export * from './types';
 export { store } from './store';

cdaff915f158dbfbc139ac3504065030632bfe26
Jeremy Purser <jeremypurser@gmail.com>
2020-02-26 20:11:47 -0500

    Add chord name slice to store

--- src/state/store.tsx
+++ src/state/store.tsx
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { guitarNeckSlice } from './guitar-neck-slice';
+import { chordNameSlice } from './chord-name-slice';
 
 const reducer = {
   guitarNeck: guitarNeckSlice.reducer,
+  chordName: chordNameSlice.reducer,
 };
 
 export const store = configureStore({ reducer });

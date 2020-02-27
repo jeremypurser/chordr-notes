7cc41a2478724987a4f6e7e996adcf46007ae4ce
Jeremy Purser <jeremypurser@gmail.com>
2020-02-26 20:12:39 -0500

    Pull in state

--- src/components/ChordDetailsForm.tsx
+++ src/components/ChordDetailsForm.tsx
@@ -1,8 +1,18 @@
 import React, { MouseEvent } from 'react';
 import { default as SaveButton } from './Button';
 import '../styles/ChordDetailsForm.scss';
+import { useDispatch, useSelector } from 'react-redux';
+import { chordNameSlice, State } from '../state';
 
 export default function ChordDetailsForm() {
+  const dispatch = useDispatch();
+  const chordName = useSelector((state: State) => state.chordName);
+  const { actions } = chordNameSlice;
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(actions.enterChordName(e.target.value));
+  };
+
   const handleSubmit = (e: MouseEvent) => {
     console.log(e);
   };
@@ -11,9 +21,15 @@ export default function ChordDetailsForm() {
     <div className="field">
       <label className="label">Chord Name</label>
       <div className="control details">
-        <input className="input" type="text" placeholder="G major" />
+        <input
+          onChange={handleChange}
+          className="input"
+          type="text"
+          value={chordName}
+          placeholder="G major"
+        />
       </div>
-      <SaveButton clickHandler={handleSubmit} text="Save" />
+      <SaveButton clickHandler={handleSubmit} text="Save Chord" />
     </div>
   );
 }

1b44e66a8554e38813f4e2b5356e73b7a954269c
Jeremy Purser <jeremypurser@gmail.com>
2020-02-26 21:10:30 -0500

    Use tuning slice for store

--- src/state/store.tsx
+++ src/state/store.tsx
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { guitarNeckSlice } from './guitar-neck-slice';
 import { chordNameSlice } from './chord-name-slice';
+import { tuningSlice } from './tuning-slice';
 
 const reducer = {
   guitarNeck: guitarNeckSlice.reducer,
   chordName: chordNameSlice.reducer,
+  tuning: tuningSlice.reducer,
 };
 
 export const store = configureStore({ reducer });

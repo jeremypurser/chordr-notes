eb82185d90e0613ba55f73e84031ed0a71a6c9c4
Jeremy Purser <jeremypurser@gmail.com>
2020-02-27 21:11:58 -0500

    Export tuning slice

--- src/state/index.ts
+++ src/state/index.ts
@@ -3,5 +3,6 @@ export {
   initialState as guitarNeckState,
 } from './guitar-neck-slice';
 export { chordNameSlice } from './chord-name-slice';
+export { tuningSlice } from './tuning-slice';
 export * from './types';
 export { store } from './store';

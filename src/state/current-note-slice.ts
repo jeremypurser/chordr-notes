9152d50a130874f9e0ab9e2a4ed7eca03de8afd5
Jeremy Purser <jeremypurser@gmail.com>
2020-02-29 15:38:26 -0500

    Refactor consolidate slice

--- src/state/current-note-slice.ts
+++ src/state/current-note-slice.ts
@@ -1,37 +1,71 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { Neck, PressStringPayload } from './types';
+import { Neck, PressStringPayload, Tuning, ChangeTuningPayload } from './types';
 
-export const initialState: Neck = [
-  ['S', 'S', 'S', 'S', 'S', 'S'],
-  ['S', 'S', 'S', 'S', 'S', 'S'],
-  ['S', 'S', 'S', 'S', 'S', 'S'],
-  ['S', 'S', 'S', 'S', 'S', 'S'],
-  ['S', 'S', 'S', 'S', 'S', 'S'],
-  ['S', 'S', 'S', 'S', 'S', 'S'],
-];
+interface CurrentNoteState {
+  name: string;
+  note: Neck;
+  tuning: Tuning;
+}
 
-export const guitarNeckSlice = createSlice({
-  name: 'guitarNeck',
+export const initialState: CurrentNoteState = {
+  name: '',
+  note: [
+    ['S', 'S', 'S', 'S', 'S', 'S'],
+    ['S', 'S', 'S', 'S', 'S', 'S'],
+    ['S', 'S', 'S', 'S', 'S', 'S'],
+    ['S', 'S', 'S', 'S', 'S', 'S'],
+    ['S', 'S', 'S', 'S', 'S', 'S'],
+    ['S', 'S', 'S', 'S', 'S', 'S'],
+  ],
+  tuning: ['E', 'A', 'D', 'G', 'B', 'E'],
+};
+
+export const currentNoteSlice = createSlice({
+  name: 'current-note',
   initialState,
   reducers: {
-    pressString: (state, action: PayloadAction<PressStringPayload>): Neck => {
+    pressString: (
+      state,
+      action: PayloadAction<PressStringPayload>
+    ): CurrentNoteState => {
       // Clone state argument, immutability
-      const newState = state.map(row => row.slice());
+      const newNote = state.note.map(row => row.slice());
       const { fret, string } = action.payload;
 
       // Only one fret can be 'R' per string
-      newState.forEach(fret => {
+      newNote.forEach(fret => {
         fret[action.payload.string] = 'S';
       });
 
       // pressString event toggles on and off
-      if (state[fret][string] === 'S') {
-        newState[fret][string] = 'R';
+      if (state.note[fret][string] === 'S') {
+        newNote[fret][string] = 'R';
       } else {
-        newState[fret][string] = 'S';
+        newNote[fret][string] = 'S';
       }
 
-      return newState as Neck;
+      return {
+        ...state,
+        note: newNote as Neck,
+      };
+    },
+    enterChordName: (
+      state,
+      action: PayloadAction<string>
+    ): CurrentNoteState => ({
+      ...state,
+      name: action.payload,
+    }),
+    changeTuning: (
+      state,
+      action: PayloadAction<ChangeTuningPayload>
+    ): CurrentNoteState => {
+      const newTuning = Array.from(state.tuning);
+      newTuning[action.payload.string] = action.payload.note;
+      return {
+        ...state,
+        tuning: newTuning as Tuning,
+      };
     },
   },
 });
--- src/state/store.tsx
+++ src/state/store.tsx
@@ -1,12 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { guitarNeckSlice } from './guitar-neck-slice';
-import { chordNameSlice } from './chord-name-slice';
-import { tuningSlice } from './tuning-slice';
+import { currentNoteSlice } from './current-note-slice';
 
 const reducer = {
-  guitarNeck: guitarNeckSlice.reducer,
-  chordName: chordNameSlice.reducer,
-  tuning: tuningSlice.reducer,
+  currentNote: currentNoteSlice.reducer,
 };
 
 export const store = configureStore({ reducer });

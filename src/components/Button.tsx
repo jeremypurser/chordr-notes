24c1812be88d968e19dbf501b75c06e6f4565f97
Jeremy Purser <jeremypurser@gmail.com>
2020-03-01 11:48:41 -0500

    Define color prop

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,12 +2,13 @@ import React, { MouseEvent } from 'react';
 
 interface ButtonProps {
   text: string;
+  color: 'is-primary' | 'is-warning';
   clickHandler: (e: MouseEvent) => void;
 }
-export default function Button({ text, clickHandler }: ButtonProps) {
+export default function Button({ text, color, clickHandler }: ButtonProps) {
   return (
     <div className="control">
-      <button onClick={clickHandler} className="button is-primary">
+      <button onClick={clickHandler} className={`button ${color}`}>
         {text}
       </button>
     </div>

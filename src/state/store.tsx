3cdaec0b63ec4b5494f6196a196c3cb19114c0cf
Jeremy Purser <jeremypurser@gmail.com>
2020-03-01 18:33:57 -0500

    Add new slice to store

--- src/state/store.tsx
+++ src/state/store.tsx
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { noteCollectionSlice } from './collection-slice';
 import { currentNoteSlice } from './current-note-slice';
 
 const reducer = {
   currentNote: currentNoteSlice.reducer,
+  collection: noteCollectionSlice.reducer,
 };
 
 export const store = configureStore({ reducer });

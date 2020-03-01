1ebb6a87456c91f525866796a02afa13ab2f167a
Jeremy Purser <jeremypurser@gmail.com>
2020-03-01 18:34:29 -0500

    Make API call in useEffect

--- src/components/NotesCollection.tsx
+++ src/components/NotesCollection.tsx
@@ -1,5 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import NotesAPI from '../api';
+import { State } from '../state';
+import { noteCollectionSlice } from '../state/collection-slice';
 
 export default function NotesCollection() {
+  const [cancel, setCancel] = useState(false);
+  const collection = useSelector((state: State) => state.collection);
+  const dispatch = useDispatch();
+  const { actions } = noteCollectionSlice;
+
+  console.log(collection);
+
+  useEffect(() => {
+    !cancel &&
+      NotesAPI.get('1').then(result => {
+        dispatch(actions.loadNotes(result.data));
+      });
+    return () => setCancel(true);
+  });
+
   return <div>hiii</div>;
 }

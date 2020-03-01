358517c1fbd43d86c94ed4f05585e910deb075dc
Jeremy Purser <jeremypurser@gmail.com>
2020-03-01 18:06:22 -0500

    Factor out container to parent component

--- src/components/CreateNote.tsx
+++ src/components/CreateNote.tsx
@@ -2,23 +2,19 @@ import 'bulma';
 import React from 'react';
 import ChordDetailsForm from './ChordDetailsForm';
 import GuitarNeck from './GuitarNeck';
-import Header from './Header';
 
 export default function CreateNote() {
   return (
-    <div className="container is-fluid">
-      <Header />
-      <section className="columns">
-        <div className="column is-one-fifth">
-          <GuitarNeck />
+    <section className="columns">
+      <div className="column is-one-fifth">
+        <GuitarNeck />
+      </div>
+      <div className="column is-one-fifth">
+        <div className="columns">
+          <div className="column is-one-fifth" />
+          <ChordDetailsForm />
         </div>
-        <div className="column is-one-fifth">
-          <div className="columns">
-            <div className="column is-one-fifth" />
-            <ChordDetailsForm />
-          </div>
-        </div>
-      </section>
-    </div>
+      </div>
+    </section>
   );
 }

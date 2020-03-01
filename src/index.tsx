f89ff569b628ee396136c8da583fc15943f46beb
Jeremy Purser <jeremypurser@gmail.com>
2020-03-01 11:00:59 -0500

    Use toastify

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/App';
 import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import App from './components/App';
 import { store } from './state';
 
 const mountNode = document.getElementById('root');
 
 const AppRoot = (
   <Provider store={store}>
+    <ToastContainer />
     <App />
   </Provider>
 );

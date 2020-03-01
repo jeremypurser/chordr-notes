9e458e6c4292862b1ffa78adb5f67c64ff1aff55
Jeremy Purser <jeremypurser@gmail.com>
2020-03-01 11:56:13 -0500

    Refactor for routing

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,24 +1,7 @@
 import 'bulma';
 import React from 'react';
-import ChordDetailsForm from './ChordDetailsForm';
-import GuitarNeck from './GuitarNeck';
-import Header from './Header';
+import CreateNote from './CreateNote';
 
 export default function App() {
-  return (
-    <div className="container is-fluid">
-      <Header />
-      <section className="columns">
-        <div className="column is-one-fifth">
-          <GuitarNeck />
-        </div>
-        <div className="column is-one-fifth">
-          <div className="columns">
-            <div className="column is-one-fifth" />
-            <ChordDetailsForm />
-          </div>
-        </div>
-      </section>
-    </div>
-  );
+  return <CreateNote />;
 }

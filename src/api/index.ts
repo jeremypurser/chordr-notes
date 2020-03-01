076d7e70d818d06d2648d1be8f2f90c8044e87da
Jeremy Purser <jeremypurser@gmail.com>
2020-03-01 11:48:20 -0500

    Add get method

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,22 +2,19 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 import { CurrentNoteState } from '../state';
 
-type Endpoint = 'http://localhost:3001/notes';
-
 const url = 'http://localhost:3001/notes';
 
 const NotesAPI = {
-  post: async (payload: CurrentNoteState, endpoint: Endpoint = url) => {
+  get: async (userId: string, endpoint = url) => {
+    return await axios.get(`${endpoint}/${userId}`);
+  },
+  post: async (payload: CurrentNoteState, endpoint = url) => {
     return await axios
       .post(endpoint, payload)
       .then(({ status }) => {
         if (status === 201) {
-          // do an action
-          // clear form
-          // toastify
           toast.success('Your note is saved!', { autoClose: 3000 });
         } else {
-          // do something else
           toast.error('There was an error while attempting to save your note.');
         }
       })

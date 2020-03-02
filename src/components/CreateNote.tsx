3f9bde98454669914828bedf3fd49cab97f30901
Jeremy Purser <jeremypurser@gmail.com>
2020-03-01 19:29:54 -0500

    Use container

--- src/components/CreateNote.tsx
+++ src/components/CreateNote.tsx
@@ -1,13 +1,16 @@
 import 'bulma';
 import React from 'react';
+import guitarNeckContainer from '../containers';
 import ChordDetailsForm from './ChordDetailsForm';
 import GuitarNeck from './GuitarNeck';
 
 export default function CreateNote() {
+  const GuitarNeckCreate = guitarNeckContainer(GuitarNeck);
+
   return (
     <section className="columns">
       <div className="column is-one-fifth">
-        <GuitarNeck />
+        <GuitarNeckCreate />
       </div>
       <div className="column is-one-fifth">
         <div className="columns">

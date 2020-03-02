94a0c1e1b36f64a0a83fc3ee27b1ab41bf6206f1
Jeremy Purser <jeremypurser@gmail.com>
2020-03-01 19:31:18 -0500

    Init container

---
+++ src/containers/NotesCollectionContainer.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import NotesAPI from '../api';
+import { State } from '../state';
+import {
+  noteCollectionSlice,
+  NoteCollectionState,
+} from '../state/collection-slice';
+import '../styles/NotesCollection.scss';
+
+export interface NotesCollectionProps {
+  collection: NoteCollectionState;
+}
+
+export default function notesCollectionContainer(
+  NotesCollection: React.ComponentType
+) {
+  return () => {
+    const [cancel, setCancel] = useState(false);
+    const collection = useSelector((state: State) => state.collection);
+    const dispatch = useDispatch();
+    const { actions } = noteCollectionSlice;
+
+    // console.log(collection);
+
+    useEffect(() => {
+      console.log('using effect');
+      !cancel &&
+        NotesAPI.get('1').then(result => {
+          dispatch(actions.loadNotes(result.data));
+        });
+      return () => setCancel(true);
+    }, [actions, cancel, dispatch]);
+
+    return <NotesCollection />;
+  };
+}

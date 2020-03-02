9236d7eb69c75270b6037a1141e80478b4ccca7a
Jeremy Purser <jeremypurser@gmail.com>
2020-03-01 19:51:21 -0500

    Factor out to container

--- src/components/NotesCollection.tsx
+++ src/components/NotesCollection.tsx
@@ -1,29 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import NotesAPI from '../api';
-import { State } from '../state';
-import { noteCollectionSlice } from '../state/collection-slice';
+import React from 'react';
+import { NotesCollectionProps } from '../containers/NotesCollectionContainer';
 import '../styles/NotesCollection.scss';
 import GuitarNeck from './GuitarNeck';
 
-export default function NotesCollection() {
-  const [cancel, setCancel] = useState(false);
-  const collection = useSelector((state: State) => state.collection);
-  const dispatch = useDispatch();
-  const { actions } = noteCollectionSlice;
-
-  // console.log(collection);
-
-  useEffect(() => {
-    console.log('using effect');
-    !cancel &&
-      NotesAPI.get('1').then(result => {
-        dispatch(actions.loadNotes(result.data));
-      });
-    return () => setCancel(true);
-  }, [actions, cancel, dispatch]);
-
-  const renderNotes = collection.map((props, i) => {
+export default function NotesCollection(props: NotesCollectionProps) {
+  const renderNotes = props.collection.map((props, i) => {
     return (
       <div key={i} className="collection-note">
         <GuitarNeck {...props} callback={() => {}} />

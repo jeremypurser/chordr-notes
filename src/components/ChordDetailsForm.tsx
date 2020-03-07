48d7c7d548860cfa95c1511336da51b6ea25ff57
Jeremy Purser <jeremypurser@gmail.com>
2020-03-06 20:06:40 -0500

    Push new note to collection

--- src/components/ChordDetailsForm.tsx
+++ src/components/ChordDetailsForm.tsx
@@ -1,7 +1,7 @@
 import React, { MouseEvent } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import NotesAPI from '../api';
-import { currentNoteSlice, State } from '../state';
+import { currentNoteSlice, noteCollectionSlice, State } from '../state';
 import '../styles/ChordDetailsForm.scss';
 import Button from './Button';
 import TuningSelect from './TuningSelect';
@@ -10,21 +10,26 @@ export default function ChordDetailsForm() {
   const dispatch = useDispatch();
   const currentNote = useSelector((state: State) => state.currentNote);
   const { name } = currentNote;
-  const { actions } = currentNoteSlice;
+  const noteActions = currentNoteSlice.actions;
+  const collectionActions = noteCollectionSlice.actions;
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    dispatch(actions.enterChordName(e.target.value));
+    dispatch(noteActions.enterChordName(e.target.value));
   };
 
   const handleSubmit = (e: MouseEvent) => {
     e.preventDefault();
-    NotesAPI.saveNote(currentNote);
-    dispatch(actions.clearForm());
+    NotesAPI.saveNote(currentNote).then(status => {
+      if (status) {
+        dispatch(collectionActions.saveNote(currentNote));
+        dispatch(noteActions.clearForm());
+      }
+    });
   };
 
   const handleClear = (e: MouseEvent) => {
     e.preventDefault();
-    dispatch(actions.clearForm());
+    dispatch(noteActions.clearForm());
   };
 
   return (

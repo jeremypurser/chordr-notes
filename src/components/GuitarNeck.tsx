029fcef3bfd0ca3ced28cc813a11cbab9b6ad53d
Jeremy Purser <jeremypurser@gmail.com>
2020-04-12 10:22:37 -0400

    Refactor, no variables

--- src/components/GuitarNeck.tsx
+++ src/components/GuitarNeck.tsx
@@ -4,36 +4,32 @@ import '../styles/GuitarNeck.scss';
 import { stringCases } from '../util';
 
 export default function GuitarNeck(props: GuitarNeckProps) {
-  const renderTuning = (
-    <div className="columns is-gapless">
-      {props.tuning.map((note, i) => (
-        <div key={i} className="column tuning">
-          {note}
-        </div>
-      ))}
-    </div>
-  );
-
-  const renderGuitarNeck = props.note.map((fret, fretIdx) => (
-    <div className="neck columns is-gapless" key={fretIdx}>
-      {fret.map((string, stringIdx) => (
-        <div
-          onClick={props.callback}
-          className="column v-line"
-          id={`${fretIdx}-${stringIdx}`}
-          key={stringIdx}
-        >
-          <span>{stringCases(string)}</span>
-        </div>
-      ))}
-    </div>
-  ));
-
   return (
     <>
       <h2 className="title">{props.name}</h2>
-      {renderTuning}
-      {renderGuitarNeck}
+      <div className="columns is-gapless">
+        {/* Tuning */}
+        {props.tuning.map((note, i) => (
+          <div key={i} className="column tuning">
+            {note}
+          </div>
+        ))}
+      </div>
+      {/* Guitar Neck */}
+      {props.note.map((fret, fretIdx) => (
+        <div className="neck columns is-gapless" key={fretIdx}>
+          {fret.map((string, stringIdx) => (
+            <div
+              onClick={props.callback}
+              className="column v-line"
+              id={`${fretIdx}-${stringIdx}`}
+              key={stringIdx}
+            >
+              <span>{stringCases(string)}</span>
+            </div>
+          ))}
+        </div>
+      ))}
     </>
   );
 }

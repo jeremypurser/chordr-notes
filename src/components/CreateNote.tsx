54dd382330686598a1f46359909cbbcb0cfb9673
Jeremy Purser <jeremypurser@gmail.com>
2020-04-12 10:25:07 -0400

    Change scope

--- src/components/CreateNote.tsx
+++ src/components/CreateNote.tsx
@@ -4,9 +4,9 @@ import { guitarNeckContainer } from '../containers';
 import ChordDetailsForm from './ChordDetailsForm';
 import GuitarNeck from './GuitarNeck';
 
-export default function CreateNote() {
-  const GuitarNeckCreate = guitarNeckContainer(GuitarNeck);
+const GuitarNeckCreate = guitarNeckContainer(GuitarNeck);
 
+export default function CreateNote() {
   return (
     <section className="columns">
       <div className="column is-one-fifth">
